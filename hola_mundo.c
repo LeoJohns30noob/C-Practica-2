7137ab757022a03087af4a32e18e3f233600144a
LeoJohnsGitHub <214759374+LeoJohns30noob@users.noreply.github.com>
2026-03-07 19:44:46 +0000

    Añadir programa en C con hola mundo y variables

---
+++ hola_mundo.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    //Este es un comentario 
+    printf("\nHola Mundo\n¿Como va el día?\n");
+     printf("\nEl programador\nPragmático\n");
+     char libro[] = "El programador pragmático";
+     printf("Libro: %s\n", libro);
+     char telf[] = "7731482079";
+     printf("Teléfono: %s\n", telf);
+    char color[] = "Amarillo";
+     printf("Color: %s\n", color);
+     int entero = 100;
+     printf("%d\n", entero);
+     float decimal = 1.4352345;
+     printf("%f\n", decimal);
+     
+
+
+
+
+    return 0;
+}
